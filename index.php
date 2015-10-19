ea8a8646a078143294e30c12bd9ea5eddebec7d7
abhinand <abhinand4858@gmail.com>
2015-10-19 21:00:16 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="style.css">
+    <script src="animate.js"></script>
+
+    <!-------------owlcarousel------->
+    <!-- Important Owl stylesheet -->
+    <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+
+    <!-- Default Theme -->
+    <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+
+    <!--  jQuery 1.7+  -->
+    <script src="jquery-1.9.1.min.js"></script>
+
+    <!-- Include js plugin -->
+    <script src="assets/owl-carousel/owl.carousel.js"></script>
+
+    <script>
+        $(document).ready(function() {
+
+            $("#owl-demo").owlCarousel({
+
+                autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+                items : 4,
+                itemsDesktop : [1199,3],
+                itemsDesktopSmall : [979,3]
+
+            });
+
+        });
+
+    </script>
+</head>
+
+
+
+<body>
+
+<! ----------Navigation--------->
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<!--------Jumbotron---------------->
+<div class="container-fluid">
+    <div class="jumbotron">
+        <i class="fa fa-shopping-cart fa-3x"></i>
+        <h1>Explore the immense collection<br> of All Brands</h1>
+
+        <p></p>
+    </div>
+</div>
+
+
+
+<!------------slide------------->
+<div class="slider">
+
+    <!----------------slide1------------->
+    <div class="slide active-slide">
+        <div class="container-fluid">
+            <div class="row">
+            <div class="slide1-body col-xs-3">
+                <h2>Choose from your favourite brands</h2><br>
+            <p>The all new collection of watches from world top brands</p>
+
+        </div>
+
+        <div class="col-xs-9">
+            <img src="http://www.formaviva.co.za/images/gallery/timepieces/06.jpg">
+    </div>
+
+
+            </div
+                </div>
+            </div>
+        </div
+
+
+
+
+
+
+<!-----------Supporting---->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+
+            <h2>One</h2>
+            <p>uibeuibciw<br>
+            bufiquvifuevif</p>
+        </div>
+
+
+    <div class="col-md-4">
+
+        <h2>One</h2>
+        <p>uibeuibci<br>
+            bufiquvifuev</p>
+    </div>
+
+
+    <div class="col-md-4">
+
+        <h2>One</h2>
+        <p>uibeuibc<br>
+            bufiq</p>
+    </div>
+</div>
+
+
+
+
+
+
+    <script src="animate.js"></script>
+</body>
+</html>
